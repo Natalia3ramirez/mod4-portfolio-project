ffe1448c15b237247f6e74ba26d6d9ab5c95b32c
Natalia Ramirez <email@example.com>
2023-06-13 15:18:54 -0700

    get all spots by spotId

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -7,7 +7,7 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
-
+//get all spots with avg rating
 router.get('/', async (req, res) => {
   const allSpots = await Spot.findAll({
     include: [
@@ -42,11 +42,84 @@ router.get('/', async (req, res) => {
   res.json({Spots: newSpots})
 })
 
-router.get('/current', requireAuth, async (req, res, next) => {
-  
+//create a spot
+
+
+// get all spots by current user
+router.get('/current', requireAuth, async (req, res) => {
+  const spots = await Spot.findAll({
+    where: {
+      ownerId: req.user.id
+    },
+    include: [
+      {
+        model: Review
+      },
+      {
+      model: SpotImage,
+      attributes: ['url']
+    }
+  ]
+  })
+  const newSpots = spots.map(spot => {
+    const spotObj = spot.toJSON();
+
+    let totalRating = 0
+    for(let review of spotObj.Reviews){
+      totalRating += review.stars
+
+    }
+    spotObj.avgRating = totalRating / spotObj.Reviews.length;
+    spotObj.previewImage = spotObj.SpotImages[0]?.url
+
+
+    delete spotObj.Reviews;
+    delete spotObj.SpotImages;
+
+    return spotObj
+  });
+
+  res.json({Spots: newSpots})
 })
 
 
+// get all spot details by id
+router.get('/:spotId', async (req, res, next) => {
+  const singleSpot = await Spot.findByPk(req.params.spotId, {
+    include: [
+      {
+        model: Review
+      },
+      {
+        model: User,
+        attributes: ['id', 'firstName', 'lastName'],
+        as: 'Owner'
+      },
+      {
+        model: SpotImage,
+        attributes: ['id', 'url', 'preview']
+      },
+    ]
+  });
+
+  if(!singleSpot) {
+    return res.status(404).json({message: "Spot couldn't be found"})
+  }
+
+  const spotObj = singleSpot.toJSON();
+
+    let totalRating = 0
+    for(let review of spotObj.Reviews){
+      totalRating += review.stars
+
+    }
+    spotObj.numReviews = spotObj.Reviews.length;
+    spotObj.avgStarRating = totalRating / spotObj.Reviews.length;
+
+    delete spotObj.Reviews;
+
+  res.json(spotObj)
+})
 
 
 
