bc6293490c065bf312c560c3d4171dce344f24ee
Natalia Ramirez <email@example.com>
2023-06-29 20:47:05 -0700

    phase 1 complete

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -3,6 +3,7 @@ import { csrfFetch } from "./csrf";
 const SET_USER = "session/setUser";
 const REMOVE_USER = "session/removeUser";
 
+// action creator
 const setUser = (user) => {
   return {
     type: SET_USER,
@@ -16,6 +17,7 @@ const removeUser = () => {
   };
 };
 
+// thunk creator
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch("/api/session", {
@@ -40,8 +42,19 @@ export const logOut = () => async (dispatch) => {
   return response;
 };
 
+export const restoreUser = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session");
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+//selectors
+
 const initialState = { user: null };
 
+
+//reducer
 const sessionReducer = (state = initialState, action) => {
   let newState;
   switch (action.type) {
