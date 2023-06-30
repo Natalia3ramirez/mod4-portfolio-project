8a99899fc21e6511ab297c65269f5e2eb0d24963
Natalia Ramirez <email@example.com>
2023-06-29 21:29:03 -0700

    Navigation

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -32,13 +32,11 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
-export const logOut = () => async (dispatch) => {
-  const response = await csrfFetch("/api/session", {
-    method: "DELETE",
-
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
   });
-  const data = await response.json();
-  dispatch(removeUser(data));
+  dispatch(removeUser());
   return response;
 };
 
